39f007e6be49156f11060708d64d951f1d274cdf
wayneisaacuy <wayneisaacuy@gmail.com>
2025-05-01 14:48:46 -0400

    split pipeline for prompting so that there is a separate script for preparing the data

---
+++ scripts/prepare_data.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+export PATH=$PATH:$(pwd)
+
+userName="t127556uhn"
+memory=16
+condaEnv="~/miniforge3/envs/LLMfinetune/bin/python"
+nGPU=0
+runTime='0-00:30:00'
+
+root_dir_proj=/cluster/projects/gliugroup/work_dir/wayne_uy/gitrepo/2024/LLM-notes-classification
+
+start_date='2008-01-01'
+end_date='2015-12-31'
+numeric_proba=1
+n_partitions=10
+random_sampling=1
+
+target_list=(
+    "target_hemoglobin_grade2plus"
+    "target_neutrophil_grade2plus"
+    "target_platelet_grade2plus"
+    "target_AKI_grade2plus"
+    "target_ALT_grade2plus"
+    "target_AST_grade2plus"
+    "target_bilirubin_grade2plus"
+    "target_esas_pain_3pt_change"
+    "target_esas_tiredness_3pt_change"
+    "target_esas_nausea_3pt_change"
+    "target_esas_depression_3pt_change"
+    "target_esas_anxiety_3pt_change"
+    "target_esas_drowsiness_3pt_change"
+    "target_esas_appetite_3pt_change"
+    "target_esas_well_being_3pt_change"
+    "target_esas_shortness_of_breath_3pt_change"
+    "target_death_in_30d"
+    "target_death_in_365d"
+    "target_ED_visit"
+)
+
+target_names=$(IFS=','; echo "${target_list[*]}")
+
+llama_cpp=1
+
+# few_shot_date=NA
+few_shot_date='2016-01-01'
+
+for fname in note_anchored_firstTreatmentOnly-medOnc-ConsultLetterClinic_deid note_tabular_anchored_firstTreatmentOnly-medOnc-ConsultLetterClinic_deid
+do
+
+for n_few_shot in 0 4 10 20
+do
+
+    file_name=${fname}.csv
+    save_dir=${root_dir_proj}/data/prompt_engineering/stage1/${fname}
+
+    if [ "$fname" == "note_anchored_firstTreatmentOnly-medOnc-ConsultLetterClinic_deid" ]; then
+        data_dir=${root_dir_proj}/data/note_anchored_deid
+    elif [ "$fname" == "note_tabular_anchored_firstTreatmentOnly-medOnc-ConsultLetterClinic_deid" ]; then
+        data_dir=${root_dir_proj}/data/note_tabular_anchored_deid
+    fi
+
+
+pySLURMargs.py $userName $memory $condaEnv $nGPU $runTime "../src/prompt/prepare_data.py $data_dir $file_name $start_date $end_date $few_shot_date $random_sampling $n_few_shot $numeric_proba $target_names $n_partitions"
+
+done
+done
+
---
+++ src/prompt/prepare_data.py
@@ -0,0 +1,145 @@
+import argparse
+import submitit
+from datetime import datetime
+import os
+import numpy as np
+from ml_common.util import load_table
+import pandas as pd
+
+def prepare_data(cfg):
+    """Use submitit to launch jobs in the SLURM cluster
+
+    References: 
+    - https://www.unitary.ai/articles/intro-to-multi-node-machine-learning-2-using-slurm
+    - https://github.com/facebookincubator/submitit/blob/main/docs/examples.md
+    """
+
+    # load parameters for splitting dataframe
+    n_partitions = cfg['n_partitions']
+    data_dir = cfg['data_dir']
+    df_name = cfg['file_name']
+    
+    # read dataframe
+    df = load_table(f'{data_dir}/{df_name}')
+
+    if '.parquet.gzip' in df_name:
+        file_name_no_ext = os.path.splitext(df_name)[0]
+        file_name_no_ext = os.path.splitext(file_name_no_ext)[0]
+    else:
+        file_name_no_ext = os.path.splitext(df_name)[0]
+
+    # data_dir = f"{data_dir}/{param_string}/data_partitions/"
+    data_dir = f"{data_dir}/data_partitions/{file_name_no_ext}"
+
+    os.makedirs(f"{data_dir}", exist_ok=True)
+
+    # list of targets
+    list_of_targets = cfg['target_names'].split(",")
+
+    if cfg['n_few_shot'] != 0:
+        for target in list_of_targets:
+            # find indices where target is not -1
+            df_few_shot = df.loc[df['treatment_date']<cfg['few_shot_date']].loc[df['note_summary'].notna()].loc[df[target] != -1].copy()
+            # sample a few examples from df_few_shot
+            df_few_shot_sample_pos = (
+                df_few_shot.loc[df_few_shot[target] == 1]
+                .sample(n=round(cfg['n_few_shot']/2), replace=False, random_state=42)
+                )
+            df_few_shot_sample_neg = (
+                df_few_shot.loc[df_few_shot[target] == 0]
+                .sample(n=cfg['n_few_shot']-df_few_shot_sample_pos.shape[0], replace=False, random_state=42)
+                )
+            
+            # fix this later if there is not enough samples
+            df_few_shot_sample = (pd.concat([df_few_shot_sample_pos, df_few_shot_sample_neg])
+                                    .reset_index(drop=True)
+                                    )
+            few_shot_examples_fname = f"few_shot_{target}_nfewshot_{cfg['n_few_shot']}_{cfg['few_shot_date']}.csv"
+            # if not os.path.isfile(f'{data_dir}/{few_shot_examples_fname}'):
+            df_few_shot_sample[['mrn','treatment_date','note','note_summary',target]].to_csv(f'{data_dir}/{few_shot_examples_fname}', index=False)
+
+    # restrict the data frame to time period
+    df = df[df['treatment_date'].between(cfg['start_date'], cfg['end_date'])].copy()
+
+    partition_list = np.array_split(df.index, n_partitions)
+
+    # if random sampling, change targets of discarded rows to -1
+    if cfg['random_sampling'] == 1:
+        n_class_samples = 300
+        for target in list_of_targets:
+            # randomly select n_class_samples indices
+            df_positive = df[df[target] == 1].copy()
+            positive_idxs = df_positive.sample(n=np.min([n_class_samples, df_positive.shape[0]]), 
+                                               replace=False, 
+                                               random_state=42).index
+            
+            df_negative = df[df[target] == 0].copy()
+            n_neg_samples = n_class_samples
+            if df_positive.shape[0] < n_class_samples:
+                n_neg_samples = 2*n_class_samples - df_positive.shape[0]
+            negative_idxs = df_negative.sample(n=n_neg_samples, 
+                                               replace=False, 
+                                               random_state=42).index
+            
+            # take the union of positive and negative indices
+            idxs = np.union1d(positive_idxs, negative_idxs)
+
+            # for the rest of the indices, set the target to -1
+            non_idxs = np.setdiff1d(df.index, idxs)
+
+            df.loc[non_idxs, target] = -1
+
+        # re-arrange data frame so that the number of queries across parts is roughly the same
+        non_negatives = (df[list_of_targets] != -1).astype(int)
+        df['non_negative_count'] = non_negatives.sum(axis=1)
+        df.sort_values('non_negative_count', ascending=False, inplace=True)
+        group_sums = [0] * n_partitions  # Keep track of the sum of non_negative_count in each group
+        group_assignments = []  # To store group assignment for each row
+
+        # greedily assign rows to groups
+        for idx, row in df.iterrows():
+            # Find the group with the minimum sum of non_negative_count
+            min_group_index = np.argmin(group_sums)
+            
+            # Assign the row to that group
+            group_assignments.append(min_group_index)
+            
+            # Update the sum of the assigned group
+            group_sums[min_group_index] += row['non_negative_count']
+
+        df['group'] = group_assignments
+        partition_list = [[] for _ in range(n_partitions)]
+
+        for idx, group in zip(df.index, df['group']):
+            partition_list[group].append(idx)
+        
+        # drop 'group' and 'non_negative_count' columns from df
+        df.drop(columns=['group', 'non_negative_count'], inplace=True)
+
+
+    data_path_partitions = f'{data_dir}/randomsampling{cfg["random_sampling"]}_{cfg["start_date"]}_{cfg["end_date"]}'
+    os.makedirs(data_path_partitions, exist_ok=True)
+
+    for partition_id, idxs in enumerate(partition_list):
+        partition_path = f'{data_path_partitions}/{partition_id}_{df_name}'
+        # if not os.path.isfile(partition_path):
+        if partition_path.endswith('.csv'):
+            df.loc[idxs].reset_index(drop=True).to_csv(partition_path, index=False)
+        elif partition_path.endswith(('.parquet','.parquet.gzip')):
+            df.loc[idxs].reset_index(drop=True).to_parquet(partition_path, compression='gzip', index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_dir", help="data directory", type=str)  # data directory
+    parser.add_argument("file_name", help="notes file name", type=str)  # notes file name
+    parser.add_argument("start_date", help="start date", type=str)  # start date
+    parser.add_argument("end_date", help="end date", type=str)  # end date
+    parser.add_argument("few_shot_date", help="date cut-off for few shot examples", type=str) # date cut off for few shot examples
+    parser.add_argument("random_sampling", help="random sampling", type=int)  # random sampling
+    parser.add_argument("n_few_shot", help="number of few shot examples", type=int)  # number of few shot
+    parser.add_argument("numeric_proba", help="numerical probability", type=int)  # numeric probability?
+    parser.add_argument('target_names', type=str, help='Comma-separated list of targets') # targets
+    parser.add_argument("n_partitions", help="number of partitions", type=int)  # number of partitions
+    cfg = vars(parser.parse_args())
+    prepare_data(cfg)
