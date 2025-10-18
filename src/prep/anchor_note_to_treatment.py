e0f05f1d0f1c0e79142786e52bd26c9a7d377583
wayneisaacuy <wayneisaacuy@gmail.com>
2025-10-17 22:03:08 -0400

    update anchor code

--- src/prep/anchor_note_to_treatment.py
+++ src/prep/anchor_note_to_treatment.py
@@ -251,6 +251,12 @@ def anchor_note_to_treatment(mode,
     # fill missing data that can be filled heuristically
     df_treat = fill_missing_data_heuristically(df_treat)
 
+    # collapse rare morphology and cancer sites into 'Other' category
+    if mode == 'train':
+        cancer_site_morphology_cols = df_treat.columns[df_treat.columns.str.contains("cancer_site_|morphology_")]
+        df_treat[cancer_site_morphology_cols] = df_treat[cancer_site_morphology_cols] == 2
+        df_treat = collapse_rare_categories(df_treat, catcols=['cancer_site', 'morphology'])
+
     if add_tabular_to_note:
         opis_df = pd.read_parquet(f'{opis_data_path}')
         if 'firstTreatmentOnly' in config_name:
@@ -264,12 +270,6 @@ def anchor_note_to_treatment(mode,
 
     # create missingness features
     df_treat = get_missingness_features(df_treat)
-
-    # collapse rare morphology and cancer sites into 'Other' category
-    if mode == 'train':
-        cancer_site_morphology_cols = df_treat.columns[df_treat.columns.str.contains("cancer_site_|morphology_")]
-        df_treat[cancer_site_morphology_cols] = df_treat[cancer_site_morphology_cols] == 2
-        df_treat = collapse_rare_categories(df_treat, catcols=['cancer_site', 'morphology'])
 
     # drop assessment_date column
     df_treat.drop('assessment_date', axis=1, inplace=True)
